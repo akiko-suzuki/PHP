37e44f941881ba0767de53ad5afcbe81a9d3495e
Akiko Suzuki <ak.o7zw@gmail.com>
2020-05-19 14:09:34 +0900

    edit/form01

--- form01/input.php
+++ form01/input.php
@@ -3,14 +3,14 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<title>DB(データーベース)に接続していないフォーム｜入力画面</title>
+<title>データーベースに接続していないフォーム｜入力画面</title>
 <link rel="stylesheet" href="css/form.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
 <div class="container">
-<h1><i class="fa fa-envelope-open-o" aria-hidden="true"></i>入力画面</h1>
+<h1>データーベースに接続していない<br><i class="fa fa-envelope-open-o" aria-hidden="true"></i>お問い合わせフォーム</h1>
 <div class="form-main">
   <div class="form-wrapper">
         <form class="form" action="confirm.php" method="post">
